d098f45bcdf5be3c33ca0e0e76cc1951986b43e0
pgougne <paris.gougne.05@gmail.com>
2025-12-12 15:51:34 +0100

    functionnal block_based_push_swap.c

--- block_based_push_swap.c
+++ block_based_push_swap.c
@@ -11,57 +11,49 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+//here, pb seulement multiple de 3
+//here, pb seulement multiple de 3
+//here, pb seulement multiple de 3
+//here, pb seulement multiple de 3
+//here, pb seulement multiple de 3
+//here, pb seulement multiple de 3
 
-int	bubble_sort_block(int *tab, int size)
+int sort_three(int *tab, int to_low)
 {
-	int	i;
-	int	j;
-	int	k;
-	int	count;
+	int count = 0;
+	int a = tab[0];
+	int b = tab[1];
+	int c = tab[2];
+	if (to_low == 1)
+		return (0);
+	if (to_low == 2)
+	{
+		if (a < b)
+			sa(tab, 3);
+		return (1);
+	}
+	if (a < b && b < c)
+		return 0;
+	if (a > b && b < c && a < c)
+		return count + sa(tab, 3);
+	if (a < b && b > c && a > c)
+	{
+		count += rra(tab, 3);
+		count += sa(tab, 3);
+		return count;
+	}
+	if (a > b && b > c)
+	{
+		count += sa(tab, 3);
+		count += rra(tab, 3);
+		return count;
+	}
+	if (a > c && c > b)
+		return count + ra(tab, 3);
+	if (b > a && a > c)
+		return count + rra(tab, 3);
 
-	j = -1;
-	count = 0;
-	while (++j < size -1)
-	{
-		i = 0;
-		while (i < size - 1 - j)
-		{
-			k = 0;
-			while (k++ < i)
-				count += ra(tab, size);
-			if (tab[0] > tab[1])
-				count += sa(tab, size);
-			k = 0;
-			while (k++ < size - i)
-				count += ra(tab, size);
-			i++;
-		}
-	}
-	return (count);
-}
-
-void	print(int *tab_a, int *tab_b)
-{
-	int i = 0;
-	__builtin_printf("|-----------------|\n");
-	__builtin_printf("| idx     tab_a |\n");
-	__builtin_printf("|-----------------|\n");
-	if (tab_b == NULL)
-	{
-		while (i < 10)
-		{
-			__builtin_printf("|% 4d   |\n", tab_a[i]);
-			i++;
-		}
-		__builtin_printf("\n\n\n");
-		return ;
-	}
-	while (i < 10)
-	{
-		__builtin_printf("|% 4d   || % 4d   |\n", tab_a[i], tab_b[i]);
-		i++;
-	}
-	__builtin_printf("\n\n\n");
+	return count;
 }
 
 int	find_min(int *tab, int size_tab, int *idx_min)
@@ -73,114 +65,129 @@ int	find_min(int *tab, int size_tab, int *idx_min)
 	min = tab[idx_min[0]];
 	i = 0;
 	idx = i;
-	while (++i < size_tab)
+	while (i < size_tab)
 	{
 		if (tab[idx_min[i]] < min)
 		{
 			min = tab[idx_min[i]];
 			idx = i;
 		}
+		i++;
 	}
 	return (idx);
 }
 
-int *block_sort(int *tab_a, int *size_a)
+int	init_utils(int **idx_min, int *size_a)
 {
-	int     nb_block;
-	int    *tab_b;
-	int    *tab_a_cpy;
-	int     size_b = 0;
-	//int     idx;
 	int	i;
-	int	*idx_min;
-	int	min = 0;
+	int	nb_block;
 
-	tab_b = calloc(*size_a, sizeof(int));
-	if (!tab_b)
-		return (NULL);
 	i = 0;
-	while (i < *size_a)
-	{
-		tab_a_cpy[i] = tab_a[i];
-		i++;
-	}
 	nb_block = *size_a / 3;
 	if (*size_a % 3 != 0)
 		nb_block++;
-	i = 0;
-	while (nb_block > i)
-	{
-		bubble_sort_block(tab_a, 3);
-		ra(tab_a, *size_a);
-		ra(tab_a, *size_a);
-		ra(tab_a, *size_a);
-		i++;
-	}
-	__builtin_printf("echo||||||||||||||||||||||||||||||||||||||||||||||||||||");
-	print(tab_a, tab_a);
-
-	idx_min = (int *)malloc(sizeof(int) * nb_block);
-	if (!idx_min)
-		return (NULL);
-	i = 0;
+	*idx_min = malloc(sizeof(int) * nb_block);
+	if (!*idx_min)
+		return (0);
 	while (i < nb_block)
 	{
-		idx_min[i] = i*3;
+		(*idx_min)[i] = i * 3;
 		i++;
 	}
-	__builtin_printf("HERE||||||||||||||||||||||||||||||||||||||||||||||||||||");
-	print(idx_min, tab_a);
+	return (nb_block);
+}
+
+void	sort_by_blocks(int *tab_a, int *size_a)
+{
+	int	i;
+
+	i = 0;
+	while (*size_a > i)
+	{
+		sort_three(tab_a, *size_a - i);
+		ra(tab_a, *size_a);
+		ra(tab_a, *size_a);
+		ra(tab_a, *size_a);
+		i += 3;
+	}
+}
+
+int	go_to_idx(int *tab_a, int *idx_min, int *size_a, int nb_block)
+{
+	int	min;
+	int	i;
+
+	min = find_min(tab_a, 3, idx_min);
+	i = 0;
+	while (i < idx_min[min])
+	{
+		ra(tab_a, *size_a);
+		i++;
+	}
+	while (++min < nb_block)
+		idx_min[min] = idx_min[min] - 1;
+	return (i);
+}
+
+void	print(int *tab_a, int *tab_b)
+{
+	int i;
+	i = 0;
+	__builtin_printf("|-----------------|\n");
+	__builtin_printf("| idx     tab_a |\n");
+	__builtin_printf("|-----------------|\n");
+	while (i < 11)
+	{
+		__builtin_printf("|% 4d   ||% 4d   |\n", tab_a[i], tab_b[i]);
+		i++;
+	}
+	__builtin_printf("\n\n\n");
+}
+
+int	*block_sort(int *tab_a, int	*tab_b, int *size_a, int *size_b)
+{
+	int	nb_block;
+	int	i;
+	int	*idx_min;
+
+	idx_min = NULL;
+	nb_block = init_utils(&idx_min, size_a);
+	if (nb_block == 0)
+		return (NULL);
+	sort_by_blocks(tab_a, size_a);
+	print(tab_a, tab_b);
 	while (*size_a != 0)
 	{
-		min = find_min(tab_a, 3, idx_min);
-		i = 0;
-		while (i < idx_min[min])
-		{
-			ra(tab_a, *size_a);
-			i++;
-		}
-		while (++min < nb_block)
-		{
-			idx_min[min] = idx_min[min]-1;
-		}
-		pb(tab_a, tab_b, size_a, &size_b);
-		while (i > 0)
-		{
+		i = go_to_idx(tab_a, idx_min, size_a, nb_block);
+		pb(tab_a, tab_b, size_a, size_b);
+		while (i-- > 0)
 			rra(tab_a, *size_a);
-			i--;
-		}
-		__builtin_printf("HERE||||||||||||||||||||||||||||||||||||||||||||||||||||");
-		print(idx_min, tab_a);
 	}
-
-	print(tab_a, tab_b);
-	while (size_b != 0)
-	{
-		pa(tab_a, tab_b, size_a, &size_b);
-	}
-	print(tab_a, tab_b);
-	free(tab_b);
+	while (*size_b != 0)
+		pa(tab_a, tab_b, size_a, size_b);
 	free(idx_min);
 	return (tab_a);
 }
 
-
 #include <stdio.h>
 int main(void)
 {
-	int tab_a[10] = {4,8,7, 3,6,1, 5,9,2};
-	int size_a = 9;
-	block_sort(tab_a, &size_a);
-	//int i = 0;
-	//__builtin_printf("|-----------------|\n");
-	//__builtin_printf("| tab_a     tab_b |\n");
-	//__builtin_printf("|-----------------|\n");
-	//while (i < size_a)
-	//{
-	//	__builtin_printf("|% 4d   || % 4d   |\n", tab_a[i], tab_b[i]);
-	//	i++;
-	//}
-	//__builtin_printf("\n\n\n");
-	//free(tab_b);
+	int tab_a[12] = {10,9,6,  4,8,5,  1,2,3,  3};
+	int tab_b[12] = {};
+	int size_a = 10;
+	int size_b = 0;
+	int i = 0;
+
+	block_sort(tab_a, tab_b, &size_a, &size_b);
+
+	printf("|-----------------|\n");
+	printf("|      tab_a      |\n");
+	printf("|-----------------|\n");
+
+	while (i < size_a)
+	{
+		printf("|% 6d    |\n", tab_a[i]);
+		i++;
+	}
 	return 0;
 }
